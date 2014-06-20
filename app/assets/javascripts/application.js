af85f587360b06b543557d1df27806585c566674
Mykola <borusyuk_kolya@ukr.net>
2014-06-20 17:04:58 +0300

    delete view using ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(function(){
+	$("#tasks, .pagination ").on("click", "a", function(){
+		$.getScript(this.href);
+		return false;
+	});
+});
